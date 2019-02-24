4aa562bd85331e2e3901af782c3d51fb0a1b311f
Marco Lopez <mlopez@syscontrollers.com>
2019-02-24 12:44:56 -0500

    eliminacion de seccion mapa

--- resources/assets/js/forms.js
+++ resources/assets/js/forms.js
@@ -1,12 +1,7 @@
 import Vue from 'vue';
-import axios from 'axios';
 import SeccionServicios from './Secciones/Servicios';
-import SeccionMapa from './Secciones/Mapa';
 import SeccionContacto from './Secciones/Contacto';
 import SeccionBlog from './Secciones/Blog';
-
-axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('#token').getAttribute('content');
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 if($("#app").length)
 {
@@ -14,7 +9,6 @@ if($("#app").length)
         el: "#app",
         components: {
             'seccion-servicios': SeccionServicios,
-            'seccion-mapa': SeccionMapa,
             'seccion-contacto': SeccionContacto,
             'seccion-blog': SeccionBlog
         }
